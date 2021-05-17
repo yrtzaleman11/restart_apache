017b9c746d63ed16291fc8eb6ceade4122bf4747
Yaritza-Aleman <Yrtz.aleman@yahoo.com>
2021-05-16 21:57:44 -0500

    version 0.4.0

--- re.sh
+++ re.sh
@@ -1,4 +1,4 @@
-  #!/bin/bash
+ #!/bin/bash
 
 CONFIG="$1"
 COMMAND="$2"
@@ -35,14 +35,21 @@ do
       VALID_VHOSTS="${VALID_VHOSTS}\n * $VHOST"
     fi
 
-  if [ "$FILENAME" == "/etc/apache2/sites-available/${CONFIG}.conf" ]
+ if [ "$FILENAME" == "/etc/apache2/sites-available/${CONFIG}.conf" ]
   then
     # Set $FILEMATCH to true if one of those files matches an actual
- # virtual-host configuration and break the loop
+    # virtual-host configuration and break the loop
     FILEMATCH=true
     break
   fi
 done
+
+# We could not match the first argument to a virtual-host preset the user with an error
+if [ $FILEMATCH  == false ]
+then
+    echo "ERROR: ${CONFIG} is NOT a valid virtual-host. Please choose from the following ${VALID_VHOSTS} $VHOST"
+    exit 1
+fi
 
 # reload is allowed
 if [ "$COMMAND" == "reload" ] || [ "$COMMAND" == "restart" ]
@@ -52,12 +59,12 @@ then
 
     # Disable a vhost configuration
     sudo a2dissite "$CONFIG"
- sudo service apache2 "$COMMAND"
-
-    # Enable a vhost configuration
+    sudo service apache2 "$COMMAND"
+ # Enable a vhost configuration
     sudo a2ensite "$CONFIG"
     sudo service apache2 "$COMMAND"
 else
     echo "ERROR: $COMMAND is an invalid service command {restart|reload}"
     exit 1
 fi
+                       
